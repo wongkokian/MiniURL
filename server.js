105f69cfd61dbee0d58b1194f917146edc77de00
DESKTOP-BLLE56T\ianwo <ianwong97@hotmail.sg>
2022-09-13 20:53:48 +0800

    Change the aesthetics of the application and make it mobile responsive

--- server.js
+++ server.js
@@ -11,23 +11,20 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
+  const shortUrls = await ShortUrl.find().limit(10).sort({_id:-1})
+  const showTable = shortUrls.length > 0
+  res.render('index', { showTable, shortUrls: shortUrls })
 })
 
 app.post('/shortUrls', async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl })
-
   res.redirect('/')
 })
 
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
   if (shortUrl == null) return res.sendStatus(404)
-
-  shortUrl.clicks++
   shortUrl.save()
-
   res.redirect(shortUrl.full)
 })
 
